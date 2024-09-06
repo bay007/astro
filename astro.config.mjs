bbf03c59d5607ac12eaa1fda502f04e9cf2496c9
bay007 <tnt.galicia@gmail.com>
2024-09-06 00:07:34 -0600

    Remove dev config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
-import { defineConfig } from 'astro/config';
+// import basicSsl from "@vitejs/plugin-basic-ssl";
+// import { defineConfig } from "astro/config";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   vite: {
+//     plugins: [basicSsl()],
+//     server: {
+//       https: true,
+//     },
+//   },
+//   devToolbar: {
+//     enabled: true,
+//   },
+// });
+
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({});
